d32e6bb2e5b8e5ee2d3d787ed0fe512e8f67cec2
Adriano Avelino <adriano-st777@hotmail.com>
2019-01-19 11:29:36 -0200

    update subject to the class

--- exemplo_rspec_tdd/spec/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator_spec.rb
@@ -1,19 +1,17 @@
 require 'calculator'
 
-describe 'Classe Calculatora' do
-subject(:calc) { Calculator.new }
-
+describe Calculator do
   context '#sum' do
     it 'with positives numbers' do
-      result = calc.sum(5,7)
+      result = subject.sum(5,7)
       expect(result).to eq(12)
     end
     it 'with positive and negative numbers' do
-      result = calc.sum(-5,7)
+      result = subject.sum(-5,7)
       expect(result).to eq(2)
     end
     it 'with negative numbers' do
-      result = calc.sum(-5,7)
+      result = subject.sum(-5,7)
       expect(result).to eq(2)
     end
   end
