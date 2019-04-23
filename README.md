54d8540479bb8f5314331c21f965470e13abad02
Adriano Avelino <adriano-st777@hotmail.com>
2019-04-22 21:38:38 -0300

    adds POST com JSON

--- README.md
+++ README.md
@@ -1510,3 +1510,21 @@ it "show - JSON 200 OK" do
   )
 end
 ```
+## 95. POST com JSON
+- exemplo de POST:
+```rb
+it 'create - JSON' do
+  member = create(:member)
+  login_as(member, scope: :member)
+
+  headers = { "ACCEPT" => "application/json" }
+
+  customers_params = attributes_for(:customer)
+  post "/customers.json", params: { customer: customers_params }, headers: headers
+  expect(response.body).to include_json(
+    id: /\d/,
+    name: customers_params[:name],
+    email: customers_params.fetch(:email)
+  )
+end
+```
--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -26,5 +26,20 @@ RSpec.describe "Customers", type: :request do
         email: (be_kind_of String)
       )
     end
+
+    it 'create - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json" }
+
+      customers_params = attributes_for(:customer)
+      post "/customers.json", params: { customer: customers_params }, headers: headers
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers_params[:name],
+        email: customers_params.fetch(:email)
+      )
+    end
   end
 end
